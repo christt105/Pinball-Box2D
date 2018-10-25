dde694d1e15d44e729c21af4e28b1078fbb06986
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2018-10-25 22:42:08 +0200

    Force circles rebound up

--- Pinball_box2D/ModuleSceneIntro.cpp
+++ Pinball_box2D/ModuleSceneIntro.cpp
@@ -960,7 +960,7 @@ void ModuleSceneIntro::OnCollision(PhysBody* bodyA, PhysBody* bodyB)
 
 	if (bodyA == circle1 || bodyA == circle2 || bodyA == circle3) {
 		b2Vec2 force(bodyB->body->GetWorldCenter() - bodyA->body->GetWorldCenter());
-		force *= 3;
+		force *= 10;
 		bodyB->body->ApplyLinearImpulse(force, bodyB->body->GetWorldCenter(), true);
 		App->ui->score += 100;
 		App->audio->PlayFx(circle_fx);
